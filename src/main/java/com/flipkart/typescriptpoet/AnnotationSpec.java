cfff4bf3bb8458a5dbd3e9e544e0073552373287
anirudhramanan <anirudh.r@flipkart.com>
2018-01-16 15:08:36 +0530

    added licence headers

--- src/main/java/com/flipkart/typescriptpoet/AnnotationSpec.java
+++ src/main/java/com/flipkart/typescriptpoet/AnnotationSpec.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2015 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.flipkart.typescriptpoet;
 
 import javax.lang.model.element.*;
@@ -22,61 +37,6 @@ public final class AnnotationSpec {
     private AnnotationSpec(Builder builder) {
         this.type = builder.type;
         this.members = Util.immutableMultimap(builder.members);
-    }
-
-    void emit(CodeWriter codeWriter, boolean inline) throws IOException {
-        String whitespace = inline ? "" : "\n";
-        String memberSeparator = inline ? ", " : ",\n";
-        if (members.isEmpty()) {
-            // @Singleton
-            codeWriter.emit("@$T", type);
-        } else if (members.size() == 1 && members.containsKey("value")) {
-            // @Named("foo")
-            codeWriter.emit("@$T(", type);
-            emitAnnotationValues(codeWriter, whitespace, memberSeparator, members.get("value"));
-            codeWriter.emit(")");
-        } else {
-            // Inline:
-            //   @Column(name = "updated_at", nullable = false)
-            //
-            // Not inline:
-            //   @Column(
-            //       name = "updated_at",
-            //       nullable = false
-            //   )
-            codeWriter.emit("@$T(" + whitespace, type);
-            codeWriter.indent(2);
-            for (Iterator<Map.Entry<String, List<CodeBlock>>> i
-                 = members.entrySet().iterator(); i.hasNext(); ) {
-                Map.Entry<String, List<CodeBlock>> entry = i.next();
-                codeWriter.emit("$L = ", entry.getKey());
-                emitAnnotationValues(codeWriter, whitespace, memberSeparator, entry.getValue());
-                if (i.hasNext()) codeWriter.emit(memberSeparator);
-            }
-            codeWriter.unindent(2);
-            codeWriter.emit(whitespace + ")");
-        }
-    }
-
-    private void emitAnnotationValues(CodeWriter codeWriter, String whitespace,
-                                      String memberSeparator, List<CodeBlock> values) throws IOException {
-        if (values.size() == 1) {
-            codeWriter.indent(2);
-            codeWriter.emit(values.get(0));
-            codeWriter.unindent(2);
-            return;
-        }
-
-        codeWriter.emit("{" + whitespace);
-        codeWriter.indent(2);
-        boolean first = true;
-        for (CodeBlock codeBlock : values) {
-            if (!first) codeWriter.emit(memberSeparator);
-            codeWriter.emit(codeBlock);
-            first = false;
-        }
-        codeWriter.unindent(2);
-        codeWriter.emit(whitespace + "}");
     }
 
     public static AnnotationSpec get(Annotation annotation) {
@@ -137,6 +97,61 @@ public final class AnnotationSpec {
 
     public static Builder builder(Class<?> type) {
         return builder(ClassName.get(type));
+    }
+
+    void emit(CodeWriter codeWriter, boolean inline) throws IOException {
+        String whitespace = inline ? "" : "\n";
+        String memberSeparator = inline ? ", " : ",\n";
+        if (members.isEmpty()) {
+            // @Singleton
+            codeWriter.emit("@$T", type);
+        } else if (members.size() == 1 && members.containsKey("value")) {
+            // @Named("foo")
+            codeWriter.emit("@$T(", type);
+            emitAnnotationValues(codeWriter, whitespace, memberSeparator, members.get("value"));
+            codeWriter.emit(")");
+        } else {
+            // Inline:
+            //   @Column(name = "updated_at", nullable = false)
+            //
+            // Not inline:
+            //   @Column(
+            //       name = "updated_at",
+            //       nullable = false
+            //   )
+            codeWriter.emit("@$T(" + whitespace, type);
+            codeWriter.indent(2);
+            for (Iterator<Map.Entry<String, List<CodeBlock>>> i
+                 = members.entrySet().iterator(); i.hasNext(); ) {
+                Map.Entry<String, List<CodeBlock>> entry = i.next();
+                codeWriter.emit("$L = ", entry.getKey());
+                emitAnnotationValues(codeWriter, whitespace, memberSeparator, entry.getValue());
+                if (i.hasNext()) codeWriter.emit(memberSeparator);
+            }
+            codeWriter.unindent(2);
+            codeWriter.emit(whitespace + ")");
+        }
+    }
+
+    private void emitAnnotationValues(CodeWriter codeWriter, String whitespace,
+                                      String memberSeparator, List<CodeBlock> values) throws IOException {
+        if (values.size() == 1) {
+            codeWriter.indent(2);
+            codeWriter.emit(values.get(0));
+            codeWriter.unindent(2);
+            return;
+        }
+
+        codeWriter.emit("{" + whitespace);
+        codeWriter.indent(2);
+        boolean first = true;
+        for (CodeBlock codeBlock : values) {
+            if (!first) codeWriter.emit(memberSeparator);
+            codeWriter.emit(codeBlock);
+            first = false;
+        }
+        codeWriter.unindent(2);
+        codeWriter.emit(whitespace + "}");
     }
 
     public Builder toBuilder() {
--- src/main/java/com/flipkart/typescriptpoet/LineWrapper.java
+++ src/main/java/com/flipkart/typescriptpoet/LineWrapper.java
@@ -1,5 +1,19 @@
+/*
+ * Copyright (C) 2015 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.flipkart.typescriptpoet;
-
 
 import java.io.IOException;
 
@@ -13,13 +27,11 @@ final class LineWrapper {
     private final Appendable out;
     private final String indent;
     private final int columnLimit;
-    private boolean closed;
-
     /**
      * Characters written since the last wrapping space that haven't yet been flushed.
      */
     private final StringBuilder buffer = new StringBuilder();
-
+    private boolean closed;
     /**
      * The number of characters since the most recent newline. Includes both out and the buffer.
      */
